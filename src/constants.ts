407ca3face6b695d63f82b185d1a9b1a5718b56c
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2023-04-05 17:12:50 +0200

    Action

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-import { getInput } from "@actions/core"
+import * as core from "@actions/core"
 
-export const GITHUB_TOKEN = getInput("github-token");
+export const GITHUB_TOKEN = core.getInput("github-token");
 
-export const SHACL_PLAY_VERSION = getInput("version");
+export const SHACL_PLAY_VERSION = core.getInput("shacl-play-version");
 
-core.debug('Version using core.getInput', SHACL_PLAY_VERSION)
-core.debug('Version using tag', SHACL_PLAY_VERSION)
+console.log('Version using core.getInput', SHACL_PLAY_VERSION)
+console.log('Version using tag', SHACL_PLAY_VERSION)
